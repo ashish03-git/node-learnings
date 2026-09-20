8801cb6e5613609b7d86c7dd1728376e947aece5
ashish03-git <ashishyadav1261@gmail.com>
2026-09-20 22:53:30 +0530

    fix: git keep files removed

--- taskflow-backend/src/db/index.js
+++ taskflow-backend/src/db/index.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
-    console.log("MongoDB connection error", error);
+    console.error("MongoDB connection error", error);
     process.exit();
   }
 };
